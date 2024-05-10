6a5c89707e1eb574023f6ebbd195fea3da27ff14
evelinakolomoets <evelina.kolomoets@gmail.com>
2024-05-10 14:01:50 +0300

    upd step03_calculations.R

--- step03_calculations.R
+++ step03_calculations.R
@@ -1,23 +1,28 @@
-df <- read.csv('workinghours_df.csv')
+df <- read.csv("workinghours_df.csv")
 
 # удаляем переменную, которая принимает единственное значение
-df <- df[ , -which(names(df) %in% c('aaj11'))]
+df <- df[, -which(names(df) %in% c("aaj11"))]
 
-cat_names <- c('aaj4.1', 'aaj21.3', 'aaj125.2', 'aah5', 
-              'aa_educ', 'aaj6', 'aaj11.1', 
-              'aaj23', 'aaj26', 'aaj72.171', 'aaj31', 
-              'aaj66.1', 'aaj62', 'aaj66', 'aaj61', 'aaj14', 'aaj81')
+cat_names <- c(
+  "aaj4.1", "aaj21.3", "aaj125.2", "aah5",
+  "aa_educ", "aaj6", "aaj11.1",
+  "aaj23", "aaj26", "aaj72.171", "aaj31",
+  "aaj66.1", "aaj62", "aaj66", "aaj61", "aaj14", "aaj81"
+)
 
 df[cat_names] <- lapply(df[cat_names], as.factor)
-df[, -which(names(df) %in% cat_names)] <- lapply(df[-which(names(df) %in% cat_names)], as.numeric)
+df[, 
+   -which(names(df) %in% cat_names)] <- lapply(df[-which(names(df) 
+                                                         %in% cat_names)], 
+                                               as.numeric)
 
-X <- df[ , -which(names(df) %in% c('aaj6.2'))]
+X <- df[, -which(names(df) %in% c("aaj6.2"))]
 y <- df$aaj6.2
 
 ### МОДЕЛИ ###
 
 # МНК
-ols <- lm(y ~ ., data=X)
+ols <- lm(y ~ ., data = X)
 ols_summary <- summary(ols)
 
 # ММП
@@ -27,22 +32,23 @@ X_with_dummies <- X[, -which(names(df) %in% cat_names)]
 X_with_dummies[] <- lapply(X_with_dummies, as.numeric)
 
 # преобразование factor колонок в OHE
-for (cat_col in cat_names){
+for (cat_col in cat_names) {
   level_names <- levels(X[[cat_col]])
   dummy_matrix <- model.matrix(~ X[[cat_col]] - 1)
-  
+
   initial_ncol <- ncol(X_with_dummies)
   X_with_dummies <- cbind(X_with_dummies, dummy_matrix)
-  
-  # Automatically assign names to dummy variables
-  needed_colname <- paste(cat_col, '_', sep = "")
+
+  needed_colname <- paste(cat_col, "_", sep = "")
   colnames(X_with_dummies)[
     (initial_ncol + 1):ncol(X_with_dummies)
-    ] <- paste(needed_colname, level_names, sep = "")
+  ] <- paste(needed_colname, level_names, sep = "")
 }
 
+rm(cat_col, level_names, initial_ncol, needed_colname, dummy_matrix)
 
-X_with_dummies <- cbind(intercept=1, X_with_dummies)
+
+X_with_dummies <- cbind(intercept = 1, X_with_dummies)
 X_with_dummies <- as.matrix(X_with_dummies)
 
 likelihood <- function(param) {
@@ -50,36 +56,15 @@ likelihood <- function(param) {
   sigma <- param[1]
   mu <- X_with_dummies %*% beta
   sum(dnorm(y, mu, sigma, log = TRUE))
-}   
+}
 
-ml <- maxLik(likelihood, 
-             start = c(sigma=sd(y), 
-                       rep(0, ncol(X_with_dummies))
-                       # intercept=1,
-                       # aaj4.1=0,
-                       # aaj13.2=0,
-                       # aaj21.3=0,
-                       # aaj125.2=0,
-                       # aah5=0,
-                       # aa_age=0,
-                       # aa_educ=0,
-                       # aaj161.3y=0,
-                       # aaj6=0,
-                       # aaj6.0=0,
-                       # aaj11.1=0,
-                       # aaj13=0,
-                       # aaj23=0,
-                       # aaj26=0,
-                       # aaj72.171=0,
-                       # aaj31=0,
-                       # aaj66.1=0,
-                       # aaj62=0,
-                       # aaj66=0,
-                       # aaj61=0,
-                       # aaj14=0,
-                       # aaj15=0,
-                       # aaj16=0,
-                       # aaj81=0
-                       ))
+init_vars <- setNames(rep(0, ncol(X_with_dummies)), colnames(X_with_dummies))
+
+ml <- maxLik(likelihood,
+  start = c(
+    sigma = sd(y),
+    init_vars
+  )
+)
 
 ml_summary <- summary(ml)
